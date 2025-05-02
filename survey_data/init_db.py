e230714cc714dacf1f2d3291de57f617590e08d3
kavya2696 <kavya.2696@gmail.com>
2025-05-01 22:52:44 -0400

    Update init_db.py

--- survey_data/init_db.py
+++ survey_data/init_db.py
@@ -1,6 +1,10 @@
+#1. Kavya Shivakumar (G01520934)
+#2. Sehaj Gill (G01535820)
+#3. Jaanaki Swaroop P(G01502869)
+#4. Koushik Vasa (G01480627)
 from app import db, app
-
+# to allow interaction with the Flask app
 with app.app_context():
-    db.drop_all()     # ← optional: drop old table if you're rebuilding
+    db.drop_all()     # to drop old table
     db.create_all()
-    print("✅ Database tables created successfully.")
+    print("Database tables created successfully.")
